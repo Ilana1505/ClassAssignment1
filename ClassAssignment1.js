ec4f3a9f4358e81eaaf512b5dba6aa70880014eb
Ilana1505 <bilana4@cs.colman.ac.il>
2024-11-25 16:59:34 +0200

    master1

--- ClassAssignment1.js
+++ ClassAssignment1.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const postRoutes = require('./routes/postRoutes');
-const commentRoutes = require('./routes/commentRoutes');
+
+const dotenv = requaire('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-app.use(express.json());
-
-app.use('/post', postRoutes);
-app.use('/comment', commentRoutes);
-
-mongoose.connect('mongodb://localhost:27017/assignmentDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Failed to connect to MongoDB', err));
+app.get("/",(req,res) => {
+    res.send("test");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
