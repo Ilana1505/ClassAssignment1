bb2a7ea1a942b3f99db088543601a6a634ee48dc
Ilana1505 <bilana4@cs.colman.ac.il>
2024-11-25 17:21:23 +0200

    master3

--- ClassAssignment1.js
+++ ClassAssignment1.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const dotenv = requaire('dotenv').config();
-const app = express();
+const ClassAssignment1 = express();
+const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
 
-app.get("/",(req,res) => {
+ClassAssignment1.get("/",(req,res) => {
     res.send("test");
 });
 
-app.listen(PORT, () => {
+ClassAssignment1.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
