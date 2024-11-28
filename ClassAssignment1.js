779dfbd8a22c64c24abe1ee84581205f984d565a
Ilana1505 <bilana4@cs.colman.ac.il>
2024-11-28 13:54:56 +0200

    request + connect to mongoDB

--- ClassAssignment1.js
+++ ClassAssignment1.js
@@ -3,9 +3,17 @@ const ClassAssignment1 = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
 
+const connectDB = require('./config/db'); 
+const postRoutes = require('./routes/posts');
+
+connectDB(); // MongoDB -חיבור ל 
+
 ClassAssignment1.get("/",(req,res) => {
     res.send("test");
 });
+
+ClassAssignment1.use(express.json());
+ClassAssignment1.use('/posts', postRoutes);
 
 ClassAssignment1.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
